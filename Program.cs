431a73791cf963be7d4d48186acb23f2cc3dccb3
Jacob Archambault <jacob.archambault@waystar.com>
2020-02-28 00:14:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace AppConfigReaderApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Reading <appSettings> Data *****\n");
+
+            // Get our custom data from the *.config file.
+            AppSettingsReader ar = new AppSettingsReader();
+            int numbOfTimes = (int)ar.GetValue("RepeatCount", typeof(int));
+            string textColor = (string)ar.GetValue("TextColor", typeof(string));
+
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), textColor);
+
+            // now print a message correctly.
+            for (int i = 0; i < numbOfTimes; i++)
+                Console.WriteLine("Howdy!");
+            Console.ReadLine();
+        }
+    }
+}
